e9229fb84a3f45aa5d8c412e2e0b82fd7868d0e3
YongJun <91900661+37-TeoYongJun@users.noreply.github.com>
2026-05-06 11:30:03 +0800

    yongjun change boi script print

--- Assets/Scripts/Boi.cs
+++ Assets/Scripts/Boi.cs
@@ -5,7 +5,7 @@ public class Boi : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("SFYIKSDGFHIYSDGFIYUSFSGVJIFVYUISDFIOVYUIOSDFGYUIOSDGFYIOIOGYSDFGIYO");
+        print("oh");
     }
 
     // Update is called once per frame
