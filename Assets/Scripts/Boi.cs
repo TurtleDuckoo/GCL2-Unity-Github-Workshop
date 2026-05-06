6a8e4b0ec5ae47387640e530a7d12f639a71d799
CSuraj6424 <CSURAJ64.24@ichat.sp.edu.sg>
2026-05-06 11:31:44 +0800

    boi
    change bois cript sagain

--- Assets/Scripts/Boi.cs
+++ Assets/Scripts/Boi.cs
@@ -5,7 +5,7 @@ public class Boi : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("oh");
+        print("sdovudsioufbhsiodfghsdfioghdsioh");
     }
 
     // Update is called once per frame
